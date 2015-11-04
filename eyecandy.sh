afe98b528a3e5c63dcaa9610ec0d9f9df5a77abc
schrooms <schrooms@gmail.com>
2015-11-04 21:41:20 +0100

    added users info

--- eyecandy.sh
+++ eyecandy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 BLUE=\\e[34m
-printf "$BLUE    //////\n"
-printf "$BLUE   { 0 0 } \n"
-printf "$BLUE    '|||'  \n"
-printf "$BLUE  oo00 00oo\n"
+
+US="$(users)"
+# lolz
+printf "Users: %s\n\n" $US 
+printf "$BLUE   //////\n"
+printf "$BLUE  { 0 0 } \n"
+printf "$BLUE   '|||'  \n"
+printf "$BLUE oo00 00oo\n"
